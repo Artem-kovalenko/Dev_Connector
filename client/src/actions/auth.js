4d2570d087350a4e249906f6f30a4d658336d12d
Artem-kovalenko <artemdev0109@gmail.com>
2020-08-20 20:24:54 +0300

    Load user & Send auth token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,9 +2,31 @@ import axios from 'axios';
 import { setAlert } from "./alert";
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from "./types";
+import setAuthToken from "../utils/setAuthToken";
 
+// Load User
+export const loadUser = () => async dispatch => {
+    if(localStorage.token) {
+        setAuthToken(localStorage.token)
+    }
+    console.log('reched')
+    try {
+        const res = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        })
+    } catch (e) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -34,3 +56,5 @@ export const register = ({ name, email, password }) => async dispatch => {
         })
     }
 };
+
+
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    AUTH_ERROR,
+    USER_LOADED
 } from "../actions/types";
 
 const initialState = {
@@ -14,6 +16,13 @@ export default function (state = initialState, action) {
     const { type, payload } = action;
 
     switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -23,6 +32,7 @@ export default function (state = initialState, action) {
                 loading: false
             }
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return {
                 ...state,
